850b1993ffe5d9ddee54305ed65494e55ce57e39
ouham002 <you@example.com>
2025-10-08 21:34:27 +0200

    korrigiert

--- pk/FirstProgramm.java
+++ pk/FirstProgramm.java
@@ -3,12 +3,11 @@ public class FirstProgramm {
 
    public static void maleTreppe(int hoehe, int stufentiefe) {
         for (int i = 1; i <= hoehe; i++) {
-            // Anzahl der Sterne pro Zeile = i * stufentiefe
+            
             for (int j = 1; j <= i * stufentiefe; j++) {
                 System.out.print("*");
             }
-            System.out.println(); // Zeilenumbruch nach jeder Stufe
-        }
+            System.out.println(); }
     }
 
     public static void main(String[] args) {
