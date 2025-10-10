299c9be48fb1bd689a430e6416ec9854fe1baa09
ouham002 <you@example.com>
2025-10-10 12:41:42 +0200

    EndProdukt

--- pk/FirstProgramm.java
+++ pk/FirstProgramm.java
@@ -1,19 +1,27 @@
 package pk;
+
 public class FirstProgramm {
 
-   public static void maleTreppe(int hoehe, int stufentiefe) {
+    public static void maleTreppe(int hoehe, int stufentiefe) {
         for (int i = 1; i <= hoehe; i++) {
-            
-            for (int j = 1; j <= i * stufentiefe; j++) {
+            // Leerzeichen vor den Sternen
+            for (int s = 0; s < (hoehe - i) * stufentiefe; s++) {
+                System.out.print(" ");
+            }
+            // Sterne
+            for (int j = 0; j < i * stufentiefe; j++) {
                 System.out.print("*");
             }
-            System.out.println(); }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        maleTreppe(6, 2); 
+        maleTreppe(6, 2);
     }
 }
+
+
 
 
 
