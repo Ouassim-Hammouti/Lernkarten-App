2d87504b9d0dbf34ee21a05941edeb2ae33463a7
ouham002 <you@example.com>
2025-10-10 12:44:28 +0200

    PRAKTIKUM 1

--- pk/FirstProgramm.java
+++ pk/FirstProgramm.java
@@ -4,11 +4,11 @@ public class FirstProgramm {
 
     public static void maleTreppe(int hoehe, int stufentiefe) {
         for (int i = 1; i <= hoehe; i++) {
-            // Leerzeichen vor den Sternen
+            
             for (int s = 0; s < (hoehe - i) * stufentiefe; s++) {
                 System.out.print(" ");
             }
-            // Sterne
+        
             for (int j = 0; j < i * stufentiefe; j++) {
                 System.out.print("*");
             }
@@ -19,6 +19,7 @@ public class FirstProgramm {
     public static void main(String[] args) {
         maleTreppe(6, 2);
     }
+
 }
 
 
